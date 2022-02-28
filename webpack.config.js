1b776edb5eba7ece037ee3177d4b560c57303641
Sam Davidoff <sam@mfbtech.com>
2022-02-28 23:28:14 +0000

    cleanup Icon

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path")
+const path = require("path");
 
 module.exports = {
   entry: "./src/index.ts",
@@ -8,35 +8,28 @@ module.exports = {
       {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
-        use: [{ loader: "@svgr/webpack", options: { typescript: true } }]
+        use: [{ loader: "@svgr/webpack", options: { typescript: true } }],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader"
-        ]
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.tsx?$/,
         use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
     filename: "align-icons.js",
     path: path.resolve(__dirname, "dist"),
     library: {
       name: "align-icons",
-      type: "umd"
-    }
-  }
-}
+      type: "umd",
+    },
+  },
+};
